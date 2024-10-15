ac5b3201b5f694232da632137bcc75bb0dcd50c4
dani <lofutty23xx@gmail.com>
2024-10-15 09:14:59 +0200

    asd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-anton)']
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
