bdf86789ecfa1c3da94120bbbd31868b62f5423c
dani <lofutty23xx@gmail.com>
2024-10-24 17:17:59 +0200

    nalam jo

--- src/app/board/board.component.tsx
+++ src/app/board/board.component.tsx
@@ -17,8 +17,10 @@ import { BoardProps, useBoardDnd } from '@/app/board/board.dnd.helpers';
 import { CardComponent } from '@/app/components/card/card.component';
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient('https://uuxantmzdfqaqqkyrtqz.supabase.co/',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1eGFudG16ZGZxYXFxa3lydHF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3NzI5MTcsImV4cCI6MjA0NTM0ODkxN30.tgIfaKgO2s-H8oiMV9AjXVPUnB7evzv29sCwOnsZIeo');
+const supabase = createClient(
+  'https://uuxantmzdfqaqqkyrtqz.supabase.co/',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1eGFudG16ZGZxYXFxa3lydHF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3NzI5MTcsImV4cCI6MjA0NTM0ODkxN30.tgIfaKgO2s-H8oiMV9AjXVPUnB7evzv29sCwOnsZIeo',
+);
 
 export default function BoardComponent({ boardCards, opponentBoardCards, pCards, pDeck, player }: BoardProps) {
   const [, startTransition] = useTransition();
@@ -42,17 +44,12 @@ export default function BoardComponent({ boardCards, opponentBoardCards, pCards,
       .on(
         'postgres_changes',
         {
-          event: 'UPDATE',
+          event: '*',
           schema: 'public',
-          table: 'battle'
         },
-        (payload) => console.log(payload)
+        (payload) => console.log(payload),
       )
       .subscribe();
-
-    return () => {
-      // supabase.removeSubscription(subscription);
-    };
   }, []);
 
   const {
@@ -85,15 +82,19 @@ export default function BoardComponent({ boardCards, opponentBoardCards, pCards,
         >
           <OpponentArea cards={[]} />
           <div
-            className={'bg-stone-700 flex-col flex-grow content-center w-full p-4 box-border flex justify-center shadow-[0px_4px_6px_0px_rgba(0,_0,_0,_0.1)] rounded-md'}>
+            className={
+              'bg-stone-700 flex-col flex-grow content-center w-full p-4 box-border flex justify-center shadow-[0px_4px_6px_0px_rgba(0,_0,_0,_0.1)] rounded-md'
+            }
+          >
             <div className={'flex flex-row gap-4 min-h-72 w-full justify-center items-center'}>
               {opponentBoardCards?.map((card: Card) => <CardComponent key={card.id} card={card} />)}
             </div>
             <BattleArea cards={items.boardCards || []} />
           </div>
           <PlayerArea cards={items.pCards || []} player={player} deck={pDeck} />
-          <DragOverlay>{act ?
-            <DraggableOverlayCardItem id={act.id} dragDelta={dragDelta} image={activeImage} /> : null}</DragOverlay>
+          <DragOverlay>
+            {act ? <DraggableOverlayCardItem id={act.id} dragDelta={dragDelta} image={activeImage} /> : null}
+          </DragOverlay>
         </DndContext>
         <div className={'flex'}>
           <Button isLoading={isPendingRestart} size={'md'} onPress={restart} color="primary">
