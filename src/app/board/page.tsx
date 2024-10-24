60ea4e0d32131e4b549037160b9af80144f6f811
dani <lofutty23xx@gmail.com>
2024-10-24 15:54:05 +0200

    superbase / prisma

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import BoardComponent, { BoardProps } from '@/app/board/board.component';
 import { fetchBoardData } from '@/app/board/board.fetcher';
+import { prisma } from '../../../prisma';
 
 export default async function Page() {
-  const data: BoardProps = await fetchBoardData() as BoardProps;
-  return (
-    <BoardComponent {...data} />
-  );
+  const data: BoardProps = (await fetchBoardData()) as BoardProps;
+
+  const stream = await prisma.battle.stream({ name: 'battle-stream' });
+
+  for await (const event of stream) {
+    console.log('New event:', event);
+  }
+
+  return <BoardComponent {...data} />;
 }
--- src/seed.ts
+++ src/seed.ts
@@ -1,15 +1,46 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient, Rarity } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.resource.create({
-    data: {
-      type: 'SILVER_BULLET',
-      quantity: 5,
-      playerId: 1,
+  const cards = [
+    {
+      name: 'Fireball',
+      description: 'A powerful fire spell.',
+      image: '/images/fireball.png',
+      ability: { damage: 30, manaCost: 20 },
+      health: 50,
+      attack: 30,
+      rarity: Rarity.EPIC,
+      tier: 2,
     },
-  });
+    {
+      name: 'Ice Blast',
+      description: 'A freezing attack that slows the enemy.',
+      image: '/images/iceblast.png',
+      ability: { damage: 20, freezeDuration: 5 },
+      health: 40,
+      attack: 20,
+      rarity: Rarity.RARE,
+      tier: 3,
+    },
+    {
+      name: 'Shield Block',
+      description: 'Defensive maneuver that blocks attacks.',
+      image: '/images/shieldblock.png',
+      ability: { blockAmount: 40 },
+      health: 100,
+      attack: 10,
+      rarity: Rarity.COMMON,
+      tier: 1,
+    },
+  ];
+
+  for (const card of cards) {
+    await prisma.card.create({
+      data: card,
+    });
+  }
 
   console.log('Adatok sikeresen feltöltve.');
 }
